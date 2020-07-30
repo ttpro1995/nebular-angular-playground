bd1df276974f5a86cbb7e1ca676d2b893bae44d4
Thai Thien <ttpro1995@yahoo.com.vn>
2020-07-30 19:00:26 +0700

    tab page not work as expect

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import {NeuPageComponent} from './neu-page/neu-page.component';
 import {TablePageComponent} from './table-page/table-page.component';
 import {CardPageComponent} from './card-page/card-page.component'
+import {TabPageComponent} from './tab-page/tab-page.component';
 
 const routes: Routes = [
   { path: 'neupage', component: NeuPageComponent },
   { path: 'tablepage', component: TablePageComponent },
   { path: 'cardpage', component: CardPageComponent },
+  { path: 'tabpage', component: TabPageComponent}
 ];
 
 @NgModule({
---
+++ src/app/tab-page/tab-page.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tab-page',
+  templateUrl: './tab-page.component.html',
+  styleUrls: ['./tab-page.component.scss']
+})
+export class TabPageComponent implements OnInit {
+
+  constructor() { }
+
+  tabs = [
+    {
+      title: 'Route neu page #1',
+      route: '/neupage',
+      icon: 'home',
+      responsive: true, // hide title before `route-tabs-icon-only-max-width` value
+    },
+    {
+      title: 'Route table page #2',
+      route: '/tablepage',
+      }
+    ];
+
+  ngOnInit(): void {
+  }
+
+}
